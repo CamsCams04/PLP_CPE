e92863a30c925b8bfb26886c0913421b0d2b9527
C_Morfin <morfincamille@gmail.com>
2025-11-06 13:47:54 +0100

    add exercise 1.1 to 1.3

---
+++ Groupe1/TP1/src/cercle.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <math.h>
+
+double getAir(double rayon) {
+    double air = rayon * rayon * M_PI;
+    return air;
+};
+
+double getPerimetre(double rayon, double air) {
+    double perimetre = 2 * rayon * M_PI;
+    return perimetre;
+}
+
+int main() {
+    /* --- Définition des variables --- */
+    double rayon;
+    double air;
+    double perimetre;
+    
+    /* --- Récupération du rayon --- */
+    printf("Veuillez saisir le rayon du cercle :\n ");
+    scanf("%lf", &rayon);
+
+    printf("Vous avez choisi le rayon %lf\n", rayon);
+
+    /* --- Calcul de l'air --- */
+    air = getAir(rayon);
+    printf("L'air de votre cercle est %lf\n", air);
+
+    /* --- Calcul du perimètre --- */
+    perimetre = getPerimetre(rayon, air);
+    printf("Le perimetre de votre cercle est %lf\n", perimetre);
+
+    return 0;
+};
