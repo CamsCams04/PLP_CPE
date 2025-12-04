7944f688e3168d233a965c5d11b3a3a27846b943
C_Morfin <morfincamille@gmail.com>
2025-12-04 14:55:40 +0100

    gestion erreur de frappe

--- Groupe1/TP3/src/repl.c
+++ Groupe1/TP3/src/repl.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 #include <string.h>
-#include <ctype.h>
-#include <stdlib.h>
 #include "date_utils.h"
+#include "utils.h"
 
 // Enumeration des différentes commandes
 enum Functions{
@@ -21,53 +20,6 @@ struct Commande
     char lang[255];
 };
 
-/* --- Remplace quelques lettres accentuées communes par leur équivalent ASCII. --- */
-void strip_accents_inplace(char *s) {
-    unsigned char *p = (unsigned char*)s;
-    while (*p) {
-        switch (*p)
-        {
-        case 0xC3: {
-            unsigned char next = *(p+1);
-            if (next) {
-                if (next == 0xA9) { *p = 'e'; /* é (C3 A9) -> e */
-                    memmove(p+1, p+2, strlen((char*)p+2)+1); 
-                    continue;
-                }
-                if (next == 0xA8) { *p = 'e'; memmove(p+1, p+2, strlen((char*)p+2)+1); continue; } /* è */
-                if (next == 0xAA) { *p = 'e'; memmove(p+1, p+2, strlen((char*)p+2)+1); continue; } /* ê approximé */
-                if (next == 0xA0) { *p = 'a'; memmove(p+1, p+2, strlen((char*)p+2)+1); continue; } /* à */
-                if (next == 0xA2) { *p = 'a'; memmove(p+1, p+2, strlen((char*)p+2)+1); continue; } /* â */
-                if (next == 0xB4) { *p = 'o'; memmove(p+1, p+2, strlen((char*)p+2)+1); continue; } /* ó approximé */
-                if (next == 0xA9) { *p = 'e'; memmove(p+1, p+2, strlen((char*)p+2)+1); continue; }
-                if (next == 0xA3) { *p = 'u'; memmove(p+1, p+2, strlen((char*)p+2)+1); continue; } /* ù */
-                if (next == 0xA7) { *p = 'c'; memmove(p+1, p+2, strlen((char*)p+2)+1); continue; } /* ç */
-                /* Si non reconnu, on passe au caractère suivant */
-            }
-            break;
-        }
-        default:
-            p++;
-        }
-    }
-}
-
-/* --- Transformation en minuscule --- */
-void to_lower_case(char *s) {
-    for( ; *s; ++s) *s = (char)tolower((unsigned char)*s);
-}
-
-/* --- Comparaison insensible à la casse --- */
-int equals_normalized(const char *a, const char *b) {
-    char tmpa[256], tmpb[256];
-    strncpy(tmpa, a, sizeof(tmpa)-1); tmpa[sizeof(tmpa)-1] = 0;
-    strncpy(tmpb, b, sizeof(tmpb)-1); tmpb[sizeof(tmpb)-1] = 0;
-    to_lower_case(tmpa);
-    to_lower_case(tmpb);
-    strip_accents_inplace(tmpa);
-    strip_accents_inplace(tmpb);
-    return strcmp(tmpa, tmpb) == 0;
-}
 
 
 /**
@@ -202,7 +154,7 @@ int main()
                 afficher_date();
                 break;
             default:
-                printf("Commande non reconnue. Tapez 'aide' pour la liste des commandes.\n");
+                gestion_erreur(commande);
                 break;
         }
         printf("\n");
