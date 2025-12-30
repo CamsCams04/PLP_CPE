07211fafaf816d89e49d10303f2e90fd98db803b
C_Morfin <morfincamille@gmail.com>
2025-12-30 11:55:49 +0100

    ajout de la gestion des parenthèses

--- Groupe1/TP4/src/infix_to_postfix.c
+++ Groupe1/TP4/src/infix_to_postfix.c
@@ -25,43 +25,75 @@ static int priority(char op) {
 /* ------------------------------ */
 
 char *infix_to_postfix(char **tokens, int count) {
-    char op_stack[MAX_STACK];
+    char op_stack[100];
     int top = -1;
 
-    char *output = malloc(MAX_OUTPUT);
+    char *output = malloc(256);
     if (!output) return NULL;
     output[0] = '\0';
 
     for (int i = 0; i < count; i++) {
         char *tok = tokens[i];
 
-        //  Si un nombre -> sortie directe
+        /* --- Si un nombre --- */
         char *end;
         strtod(tok, &end);
         if (*end == '\0') {
             strcat(output, tok);
             strcat(output, " ");
+            continue;
         }
-        // Si opérateur
-        else if (strlen(tok) == 1 && strchr("+-*/", tok[0])) {
-            char op = tok[0];
 
-            while (top >= 0 && priority(op_stack[top]) >= priority(op)) {
+        /* --- Si une parenthèse ouvrante --- */
+        if (strcmp(tok, "(") == 0) {
+            op_stack[++top] = '(';
+            continue;
+        }
+
+        /* -- Si parentèse fermante --- */
+        if (strcmp(tok, ")") == 0) {
+            while (top >= 0 && op_stack[top] != '(') {
+                char tmp[2] = { op_stack[top--], '\0' };
+                strcat(output, tmp);
+                strcat(output, " ");
+            }
+            if (top < 0) {
+                printf("Erreur : parenthèse fermante sans ouvrante\n");
+                free(output);
+                return NULL;
+            }
+            top--; // supprimer '('
+            continue;
+        }
+
+        /* --- Si opérateur --- */
+        if (strlen(tok) == 1 && strchr("+-*/", tok[0])) {
+            char op = tok[0];
+            while (top >= 0 &&
+                   op_stack[top] != '(' &&
+                   priority(op_stack[top]) >= priority(op)) {
+
                 char tmp[2] = { op_stack[top--], '\0' };
                 strcat(output, tmp);
                 strcat(output, " ");
             }
             op_stack[++top] = op;
+            continue;
         }
-        else {
-            printf("Token invalide : %s\n", tok);
+        
+        /* --- Token invalide --- */
+        printf("Token invalide : %s\n", tok);
+        free(output);
+        return NULL;
+    }
+
+    /* --- Vider la pile --- */
+    while (top >= 0) {
+        if (op_stack[top] == '(') {
+            printf("Erreur : parenth�se ouvrante non ferm�e\n");
             free(output);
             return NULL;
         }
-    }
-
-    // Vider la pile
-    while (top >= 0) {
         char tmp[2] = { op_stack[top--], '\0' };
         strcat(output, tmp);
         strcat(output, " ");
